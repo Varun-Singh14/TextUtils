bbf57a1a88a56cde0bf2c7b63ba8205d386ef19d
Varun-Singh14 <varunom123singh@gmail.com>
2025-02-28 19:02:22 +0530

    Applying CSS to Accrodion button

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -40,10 +40,7 @@ export default function TextForm(props) {
 
   // This method is used to copy the text
   const handleCopy = () => {
-    let text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    document.getSelection().removeAllRanges();
+    navigator.clipboard.writeText(text);
     props.showAlert("Copied to clipboard!", "success");
   };
 
